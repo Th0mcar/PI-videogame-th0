fabb97c9c9238dc0d373d1ffc1b7330fcc568905
Juan Diego Martínez <juan.martinez@btilab.co>
2023-08-02 13:11:57 -0500

    :memo: agregué un comentario bacano

--- api/src/controllers/genres.js
+++ api/src/controllers/genres.js
@@ -5,6 +5,7 @@ const { API_KEY } = process.env;
 // solicita de la API/almacena la info en la db
 const getGenresVg = async () => {
 
+    //soy juan, estos son los 5 tops mas dificil de oir
     let response = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
     let genres = await response.data.results.map(g => g.name)
 
